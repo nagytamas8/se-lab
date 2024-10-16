37d15773ea57bb70668b5986d8f144cd68f9cf5e
unknown <176405@BME.HU>
2024-10-16 17:10:38 +0200

    kommenteltelsohiba

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -31,7 +31,7 @@ public class TorpedoStore {
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      throw new IllegalArgumentException("numberOfTorpedos");
+      throw new IllegalArgumentException("numberOfTorpedos"); //a torpedok szamat dobjuk vissza
     }
 
     boolean success = false;
